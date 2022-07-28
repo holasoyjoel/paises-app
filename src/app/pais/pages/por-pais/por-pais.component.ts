bac23ad8ebb7ff2e86ac4e1a22700f356f04eede
Correa Joel <joelcorrea.alejandro123@gmail.com>
2022-07-28 00:08:26 -0300

    primer commit

---
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styles: [
+  ]
+})
+export class PorPaisComponent{
+  termino: string = 'hola mundo';
+  constructor(private paisService: PaisService) { }
+
+  buscar(){
+    this.paisService.buscarPais(this.termino)
+    .subscribe(resp => {
+      console.log(resp);
+    });
+  }
+
+}
